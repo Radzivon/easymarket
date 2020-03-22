6b442a88e0964d28a1afde63018af4da3e5733bc
Egor Kamysh <egorkamysh99@gmail.com>
2020-03-22 15:35:51 +0300

    create project

---
+++ src/main/java/com/easymarket/easymarket/EasymarketApplication.java
@@ -0,0 +1,13 @@
+package com.easymarket.easymarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasymarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasymarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/easymarket/easymarket/EasymarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.easymarket.easymarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasymarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
